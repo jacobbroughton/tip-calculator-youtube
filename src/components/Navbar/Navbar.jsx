e61f661e6e80f1ab25a0f42b41079c1c73cbf32a
Jacob Broughton <jlbroughton88@gmail.com>
2023-04-30 14:24:45 -0400

    initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className='nav-container'>
+        <p>Tip Calculator</p>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
